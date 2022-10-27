ba9aba743075f090166f83cbd61e4c941b333eea
Jai Sachdev <jsachdev@pppl.gov>
2022-10-27 15:13:53 -0400

    Update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,6 +31,11 @@ templates_path = ['_templates']
 
 html_theme = 'sphinx_rtd_theme'
 html_favicon = 'pppl_favicon.ico'
+html_logo = 'transp_logo.png'
+html_theme_options = {
+    'logo_only': True,
+    'display_version': False,
+}
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
